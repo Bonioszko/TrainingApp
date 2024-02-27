ba03005f9442c4babc83eef25e01182fc9cd6f19
Bonioszko <bartek.pers@gmail.com>
2024-02-27 22:03:51 +0100

    deploymeny

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,13 @@ db.on("error", console.error.bind(console, "mongo connection error"));
 const currentDirectory = process.cwd();
 const app = express();
 app.use(express.static(path.join(currentDirectory, "/frontend/dist")));
-app.get("*", (req, res) => {
-    res.sendFile(path.join(currentDirectory, "frontend", "dist", "index.html"));
-});
+
 app.use(express.json()); // for parsing application/json
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+const port = 8000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.get("/", (req, res) => {
     return res.json("test is wotrking ");
 });
@@ -24,5 +25,6 @@ app.use("/api/auth", require("./routes/authRouter"));
 app.use("/api/exercise", require("./routes/exerciseRouter"));
 app.use("/api/training", require("./routes/trainingRouter"));
 app.use("/api/trainingInstance", require("./routes/trainingInstanceRouter"));
-const port = 8000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(currentDirectory, "frontend", "dist", "index.html"));
+});
