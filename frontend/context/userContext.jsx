3002a5b02522b31167fffd8ffe02e88f6db16950
Bonioszko <bartek.pers@gmail.com>
2024-02-27 14:16:03 +0100

    localhost

--- frontend/context/userContext.jsx
+++ frontend/context/userContext.jsx
@@ -6,11 +6,15 @@ export function UserContextProvider({ children }) {
     const [user, setUser] = useState(null);
     useEffect(() => {
         if (!user) {
+            console.log();
             fetch(import.meta.env.VITE_REACT_APP_URL_API + "/auth/profile", {
                 credentials: "include", // Include this line
             })
                 .then((response) => response.json())
-                .then((data) => setUser(data));
+                .then((data) => {
+                    console.log(data);
+                    setUser(data);
+                });
         }
     }, [user]);
 
